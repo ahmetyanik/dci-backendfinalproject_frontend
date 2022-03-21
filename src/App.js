5b33768f895cc6013be5878ebe9ddece92e8cbb5
ahmetyanik <ahmetyank4242@gmail.com>
2022-03-21 10:59:38 +0100

    Card is created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Card from './components/Card';
+
+// api adress: https://api.openweathermap.org/data/2.5/weather?q=Ankara&appid=a7ccf39f58624360e151dce17c818ef3
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Card/>
     </div>
   );
 }
